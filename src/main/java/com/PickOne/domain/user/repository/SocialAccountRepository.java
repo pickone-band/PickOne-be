5ee133fdb808a36b2d00d3e4b926512fdcd38a86
haechangchoi <buraldongja@naver.com>
2025-02-07 17:44:21 +0900

    ✨[feat] 소셜 계정 연동 리포지토리 추가 - OAuth2 소셜 로그인 사용자 정보를 저장하고 관리하기 위한 리포지토리 구현

--- src/main/java/com/PickOne/domain/user/repository/SocialAccountRepository.java
+++ src/main/java/com/PickOne/domain/user/repository/SocialAccountRepository.java
@@ -5,7 +5,11 @@ import com.PickOne.domain.user.model.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
     boolean existsByMemberIdAndProvider(Long memberId, SocialProvider provider); // 소셜 계정 중복
+
+    Optional<SocialAccount> findByProviderAndProviderUserId(SocialProvider socialProvider, String providerUserId);
 }
